f92f83542eaf6e803f2ca7fd5b4af17de4bae8d1
WickedWhale <lfhong21@fisk.edu>
2016-03-19 18:08:24 -0500

    hi sign up and login

--- app/src/main/java/com/example/uhmier/fiskhacks/SignupActivity.java
+++ app/src/main/java/com/example/uhmier/fiskhacks/SignupActivity.java
@@ -2,12 +2,30 @@ package com.example.uhmier.fiskhacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.InjectView;
 
 public class SignupActivity extends AppCompatActivity {
+
+    @InjectView(R.id.editTextFirstName)
+    EditText editTextFirstName;
+    @InjectView(R.id.editTextLastName)
+    EditText editTextLastName;
+    @InjectView(R.id.editTextSignUpUsername)
+    EditText editTextSignUpUsername;
+    @InjectView(R.id.editTextSignUpPassword)
+    EditText editTextSignUpPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    private void signUp(){
+        String firstName = editTextFirstName.getText().toString();
+        String lastName = editTextLastName.getText().toString();
     }
 }
