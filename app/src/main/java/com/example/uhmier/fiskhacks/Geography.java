be2283fe86f458dd24006b50d0f2da948707329f
agauli <Programmer@agauli.local>
2016-03-19 23:54:52 -0500

    geography

--- app/src/main/java/com/example/uhmier/fiskhacks/Geography.java
+++ app/src/main/java/com/example/uhmier/fiskhacks/Geography.java
@@ -1,22 +1,45 @@
 package com.example.uhmier.fiskhacks;
+import android.location.Location;
+import android.location.Geocoder;
+import android.location.Address;
+import java.util.List;
+import com.google.android.gms.vision.barcode.Barcode.GeoPoint;
 
 /**
  * Created by Programmer on 3/19/16.
  */
 public class Geography {
 
-    public float getDistance(Location a, Location b){
+    public float getDistance(GeoPoint a, GeoPoint b){
+        Location loc1 = new Location("Location");
+        loc1.setLatitude(a.getLatitudeE6()/ 1E6);
+        loc1.setLongitude(a.getLongitudeE6()/ 1E6);
 
+        Location loc2 = new Location("Location2");
+        loc2.setLatitude(b.getLatitudeE6()/ 1E6);
+        loc2.setLongitude(b.getLongitudeE6()/ 1E6);
 
-
-
+        float distanceInmeters = loc1.distanceTo(loc2);
+        return distanceInmeters;
     }
 
+    public GeoPoint getLocationcood(String strAddress){
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        GeoPoint p1 = null;
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
 
-    public .... getGeolocation(String address){
+            p1 = new GeoPoint((int) (location.getLatitude() * 1E6),
+                    (int) (location.getLongitude() * 1E6));
 
-
-
+            return p1;
+        }
     }
-
 }
---
+++ app/src/main/java/com/example/uhmier/fiskhacks/NearestEvents.java
@@ -0,0 +1,17 @@
+package com.example.uhmier.fiskhacks;
+
+/**
+ * Created by Programmer on 3/19/16.
+ */
+public class NearestEvents {
+
+    public Event[] nearestEvents{
+
+
+
+
+    }
+
+
+
+}
