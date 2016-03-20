15447e70d7d52c9365c046b2cf84484cc9c40e98
agauli <Programmer@agauli.local>
2016-03-19 22:04:36 -0500

    Rahmi and Ashish

---
+++ app/src/main/java/com/example/uhmier/fiskhacks/Location.java
@@ -0,0 +1,21 @@
+package com.example.uhmier.fiskhacks;
+
+/**
+ * Created by Programmer on 3/19/16.
+ */
+public class Location {
+
+    public float getDistance(Location a, Location b){
+
+
+
+    }
+
+
+    public .... getGeolocation(String address){
+
+
+
+    }
+
+}
